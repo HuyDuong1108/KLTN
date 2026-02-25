74982c05e7c3a037817abb49295729841399505c
Văn ThịThanh Hương <52200128@student.tdtu.edu.vn>
2026-02-25 18:11:01 +0700

    fix lis

--- duan/macos/Flutter/GeneratedPluginRegistrant.swift
+++ duan/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,24 +5,28 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
 import cloud_firestore
 import connectivity_plus
 import firebase_auth
 import firebase_core
 import firebase_database
 import flutter_tts
+import just_audio
 import path_provider_foundation
 import record_macos
 import speech_to_text
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
